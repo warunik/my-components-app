ac151780b64c45a961c53e157ab74edc4151df30
Waruni Kekulandara <warunilkekulandara20@gmail.com>
2024-06-28 23:13:15 +0530

    Conribution

---
+++ src/shared/components/Contribution.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chart from './LineChart';
+
+const Contribution: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <Chart />
+      </div>
+    </div>
+  );
+};
+
+export default Contribution;
